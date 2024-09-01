694c5a6f3ef2695094447de3b23cd62afdf8bd93
yizhe <liaoyizhe75@gmail.com>
2024-09-01 12:58:49 +0800

    change image url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
-  image: {
-    dir: 'public/images'
-  },
   devtools: { enabled: true },
 
   // 配置插件
